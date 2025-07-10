902f6439f7593bff00c74871fc3a80749c688512
Jiří Polášek <me@jiripolasek.com>
2025-07-10 17:42:48 +0200

    Allow WindowManager to recognize and ignore UWP windows
    We can't bring them to foreground as normal desktop windows when minimized, but they look like normal non-minimized window all the time.

--- src/MediaControlsExtension/Helpers/WindowManager.cs
+++ src/MediaControlsExtension/Helpers/WindowManager.cs
@@ -13,41 +13,42 @@ internal static partial class WindowManager
 {
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    private static partial bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+    private static partial bool EnumWindows(EnumWindowsProc enumProc, nint lParam);
 
     [LibraryImport("user32.dll", SetLastError = true, EntryPoint = "GetWindowTextW", StringMarshalling = StringMarshalling.Utf16)]
-    private static unsafe partial int GetWindowText(IntPtr hWnd, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2)] char[] buffer, int nMaxCount);
+    private static unsafe partial int GetWindowText(nint hWnd, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2)] char[] buffer, int nMaxCount);
 
     [LibraryImport("user32.dll", SetLastError = true, EntryPoint = "GetWindowTextLengthW", StringMarshalling = StringMarshalling.Utf16)]
-    private static partial int GetWindowTextLength(IntPtr hWnd);
+    private static partial int GetWindowTextLength(nint hWnd);
 
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    private static partial bool IsWindowVisible(IntPtr hWnd);
+    private static partial bool IsWindowVisible(nint hWnd);
 
     [LibraryImport("user32.dll", SetLastError = true)]
-    private static partial uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+    private static partial uint GetWindowThreadProcessId(nint hWnd, out uint processId);
 
     [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    private static partial bool SetForegroundWindow(IntPtr hWnd);
+    private static partial bool SetForegroundWindow(nint hWnd);
 
     [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    private static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    private static partial bool ShowWindow(nint hWnd, int nCmdShow);
 
     [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    private static partial bool IsIconic(IntPtr hWnd);
+    private static partial bool IsIconic(nint hWnd);
 
-    [LibraryImport("user32.dll", SetLastError = true, EntryPoint = "GetClassNameW")]
-    private static partial int GetClassName(IntPtr hWnd, [Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U2)] char[] lpClassName, int nMaxCount);
+    [LibraryImport("user32.dll", EntryPoint = "GetClassNameW")]
+    private static unsafe partial int GetClassName(nint hWnd, char* lpClassName, int nMaxCount);
 
     [UnmanagedFunctionPointer(CallingConvention.Winapi)]
-    private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+    private delegate bool EnumWindowsProc(nint hWnd, nint lParam);
 
     private const int SW_RESTORE = 9;
     private const int SW_SHOW = 5;
+    private const int CLASS_NAME_CAPACITY = 256;
 
     /// <summary>
     /// Get all visible windows
@@ -115,7 +116,7 @@ internal static partial class WindowManager
     /// <summary>
     /// Get window information
     /// </summary>
-    private static WindowInfo GetWindowInfo(IntPtr hWnd)
+    private static WindowInfo GetWindowInfo(nint hWnd)
     {
         var info = new WindowInfo { Handle = hWnd };
 
@@ -142,9 +143,7 @@ internal static partial class WindowManager
             info.MainModulePath = "";
         }
 
-        var classNameBuffer = new char[256];
-        var actualClassNameLength = GetClassName(hWnd, classNameBuffer, classNameBuffer.Length);
-        info.ClassName = new string(classNameBuffer, 0, actualClassNameLength);
+        info.ClassName = GetClassNameSafe(hWnd);
 
         return info;
     }
@@ -152,15 +151,38 @@ internal static partial class WindowManager
     /// <summary>
     /// Bring window to front
     /// </summary>
-    public static bool BringWindowToFront(IntPtr hWnd)
+    public static bool BringWindowToFront(nint hWnd)
     {
+        // We can't unminimize UWP windows manually: the real window lives in ApplicationFrameHost process
+        // and we can't find the connection between app process and frame host when minimized.
+        if (IsUwpWindow(hWnd))
+        {
+            return false;
+        }
+
         if (IsIconic(hWnd))
         {
             ShowWindow(hWnd, SW_RESTORE);
         }
 
         ShowWindow(hWnd, SW_SHOW);
-        SetForegroundWindow(hWnd);
-        return true;
+        return SetForegroundWindow(hWnd);
+    }
+
+    private static bool IsUwpWindow(nint hWnd)
+    {
+        var className = GetClassNameSafe(hWnd);
+        return className.Equals("Windows.UI.Core.CoreWindow", StringComparison.Ordinal);
+    }
+
+    private static unsafe string GetClassNameSafe(nint hWnd)
+    {
+        Span<char> buffer = stackalloc char[CLASS_NAME_CAPACITY];
+
+        fixed (char* p = buffer)
+        {
+            int len = GetClassName(hWnd, p, buffer.Length);
+            return len != 0 ? new string(buffer.Slice(0, len)) : string.Empty;
+        }
     }
 }
